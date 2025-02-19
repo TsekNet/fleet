7f1c40c4ddcaff14630e207c14ab8105d5d4180d
RachelElysia <rachel@fleetdm.com>
2025-02-19 12:23:41 -0500

    Display name

--- frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareAppStoreVpp/SoftwareVppForm/SoftwareVppForm.tsx
+++ frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareAppStoreVpp/SoftwareVppForm/SoftwareVppForm.tsx
@@ -199,7 +199,7 @@ const SoftwareVppForm = ({
             graphicNames="app-store"
             fileDetails={{
               name: softwareVppForEdit.name,
-              platform: softwareVppForEdit.platform,
+              platform: PLATFORM_DISPLAY_NAMES[softwareVppForEdit.platform],
             }}
             canEdit={false}
           />
